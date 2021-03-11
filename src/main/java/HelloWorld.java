ce92854962dbe688eb15d135766ab466477e1bd0
govardhan8953 <36076095+govardhan8953@users.noreply.github.com>
2021-03-11 16:24:38 +0530

    Hi world added

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld implements SparkApplication {
 	@Override
 	public void init() {
 		get("/hello", (req, res) -> "Hello World");
+		get("/hi", (req, res) -> "Hi World");
 	}
 }
